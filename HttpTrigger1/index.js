760b333a24c26dd3da60a2ba67638f5935401fed
Akhil Karun <akhil.karun@gmail.com>
2023-10-02 12:45:08 +1100

    code cleanup

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,20 +1,11 @@
 module.exports = async function (context, req) {
     const { Octokit, App } = require("octokit");
     const express = require('express');
+    require('dotenv').config();
 
     const apiKey = process.env.API_KEY;
 
     context.log('JavaScript HTTP trigger function processed a new request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    // context.res = {
-    //     // status: 200, /* Defaults to 200 */
-    //     body: responseMessage
-    // };
 
     const app = express();
 
