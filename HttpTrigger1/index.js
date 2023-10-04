d305f46238a3de95d677d72e741600fcebbd3799
Akhil Karun <akhil.karun@gmail.com>
2023-10-04 16:43:19 +1100

    refactor code

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,77 +1,91 @@
+const { Octokit } = require("octokit");
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const apiKey = process.env.GITHUB_API_KEY;
+
+const octokit = new Octokit({ auth: apiKey });
+
 module.exports = async function (context, req) {
-    const { Octokit } = require("octokit");
-    require('dotenv').config();
-
-    const apiKey = process.env.GITHUB_API_KEY;
-
     context.log('JavaScript HTTP trigger function processed a new request.');
 
-    const octokit = new Octokit({ auth: apiKey });
-
-    if (req.method === "POST") {
-        const githubEvent = req.headers['x-github-event'];
-        const data = req.body;
-
-        context.res = {
-            status: 202,
-            body: 'Accepted'
-        };
-
-        if (githubEvent === 'repository') {
-            const action = data.action;
-            context.log('GitHub sent a repository action event');
-            if (action === 'created') {
-                context.log(`A repository was created with this title: ${data.repository.name}`);
-
-                try {
-                    const response_protection = await octokit.request('PUT /repos/{owner}/{repo}/branches/{branch}/protection', {
-                        owner: data.repository.owner.login,
-                        repo: data.repository.name,
-                        branch: 'main',
-                        headers: {
-                            'X-GitHub-Api-Version': '2022-11-28'
-                        },
-                        required_status_checks: null,
-                        enforce_admins: true,
-                        required_pull_request_reviews: null,
-                        restrictions: null
-                    });
-                    context.log('Branch protection enforced:', response_protection.data);
-                } catch (error) {
-                    context.log.error('Error enforcing branch protection:', error);
-                }
-
-                try {
-                    const response_issue = await octokit.request('POST /repos/{owner}/{repo}/issues', {
-                        owner: data.repository.owner.login,
-                        repo: data.repository.name,
-                        title: 'Branch protection enabled',
-                        body: 'Main branch is now protected @nanospeck .',
-                        assignees: ['nanospeck'],
-                        labels: ['branch-settings'],
-                        headers: {
-                            'X-GitHub-Api-Version': '2022-11-28'
-                        }
-                    });
-                    context.log('Issue created:', response_issue.data);
-                } catch (error) {
-                    context.log.error('Error creating issue:', error);
-                }
-
-            } else {
-                context.log(`Unhandled action for the repository event: ${action}`);
-            }
-        } else if (githubEvent === 'ping') {
-            context.log('GitHub sent the ping event');
-        } else {
-            context.log(`Unhandled event: ${githubEvent}`);
-        }
-    } else {
+    if (req.method !== "POST") {
         context.res = {
             status: 400,
             body: "Invalid request method. Only POST is supported."
         };
+        return;
     }
 
+    const githubEvent = req.headers['x-github-event'];
+    const data = req.body;
 
+    context.res = {
+        status: 202,
+        body: 'Accepted'
+    };
+
+    try {
+        await handleGitHubEvent(githubEvent, data);
+    } catch (error) {
+        context.log.error('Error handling GitHub event:', error);
+    }
+}
+
+async function handleGitHubEvent(event, data) {
+    if (event === 'repository') {
+        await handleRepositoryEvent(data);
+    } else if (event === 'ping') {
+        context.log('GitHub sent the ping event');
+    } else {
+        context.log(`Unhandled event: ${event}`);
+    }
+}
+
+async function handleRepositoryEvent(data) {
+    const action = data.action;
+
+    context.log('GitHub sent a repository action event');
+    if (action === 'created') {
+        const owner = data.repository.owner.login;
+        const repo = data.repository.name;
+
+        try {
+            const response_protection = await octokit.request('PUT /repos/{owner}/{repo}/branches/{branch}/protection', {
+                owner,
+                repo,
+                branch: 'main',
+                headers: {
+                    'X-GitHub-Api-Version': '2022-11-28'
+                },
+                required_status_checks: null,
+                enforce_admins: true,
+                required_pull_request_reviews: null,
+                restrictions: null
+            });
+            context.log('Branch protection enforced:', response_protection.data);
+        } catch (error) {
+            throw new Error(`Error enforcing branch protection: ${error.message}`);
+        }
+
+        try {
+            const response_issue = await octokit.request('POST /repos/{owner}/{repo}/issues', {
+                owner,
+                repo,
+                title: '🔐 Branch protection enabled',
+                body: 'Main branch is now protected @nanospeck .',
+                assignees: ['nanospeck'],
+                labels: ['branch-settings'],
+                headers: {
+                    'X-GitHub-Api-Version': '2022-11-28'
+                }
+            });
+            context.log('Issue created:', response_issue.data);
+        } catch (error) {
+            throw new Error(`Error creating issue: ${error.message}`);
+        }
+    } else {
+        context.log(`Unhandled action for the repository event: ${action}`);
+    }
 }
